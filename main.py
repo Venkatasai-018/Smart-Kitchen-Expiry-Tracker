66a7e36390cbd7483b07a430a1e253f4a6242e53
venkatasaiKommu <Venkatasai.Kommu@Kroll.com>
2025-09-19 23:53:15 +0530

    port changed

--- main.py
+++ main.py
@@ -19,4 +19,4 @@ app.include_router(delete.route)
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", port=8080,reload=True)
+    uvicorn.run("main:app",host="0.0.0.0", port=8080,reload=True)
